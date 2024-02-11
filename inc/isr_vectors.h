30c7a7a1778b8abafcec578534e99a3501b8949a
Marc-André Renaud <ma.renaud@slashvoid.com>
2024-02-10 21:40:51 -0500

    Switch system_RP2040.c to cpp

--- inc/isr_vectors.h
+++ inc/isr_vectors.h
@@ -1,11 +1,8 @@
 #ifndef ISR_VECTORS_H
 #define ISR_VECTORS_H
 
-
-#pragma once
 #include <functional>
 #include "RP2040.h"
 void registerHandler(IRQn_Type irqn, std::function<void()> &&f);
-
 
 #endif //ISR_VECTORS_H
